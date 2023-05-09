1775e26f3619138c049b1ed31ff060756ba5ae57
alujuan <juanpeterssonb@gmail.com>
2023-05-09 15:08:27 -0300

    Modificando as telas

--- App.js
+++ App.js
@@ -1,24 +1,70 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, TextInput, ScrollView } from 'react-native';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
-      <Text>Tela principal</Text>
+      <View style={styles.logo}>
+        <Text style={{ fontSize: 40}}>Veterinaria</Text>
+        < Image style={styles.perfil_foto} source={{}} />
+      </View>
+      <TextInput style={styles.textimput_cadastrologin}/>
+      <TextInput style={styles.textimput_cadastrosenha}/>
       <Button
-        title="Ip para Detalhes"
+        title="Entrar"
         onPress={() => navigation.navigate('Details')}
       />
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+    
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <View style={styles.logo}>
+        <Text style={{ fontSize: 40}}>Veterinaria</Text>
+        < Image style={styles.perfil_foto} source={{}} />
+      </View>
+      < Image style={{ resizeMode: "stretch", height: 200, width: "90%", margin: 10}}
+       source={require('./img/img1.jpeg')} 
+      />
+        <Button
+          title="Saida"
+          onPress={() => navigation.navigate('Tratamento')}
+        />
+        <Button
+          title="Calendario"
+          onPress={() => navigation.navigate('Calendario')}
+        />
+    </View>
+    
+  );
+}
+function TratamentoScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <View style={styles.logo}>
+        <Text style={{ fontSize: 40}}>Veterinaria</Text>
+        < Image style={styles.perfil_foto} source={{}} />
+      </View>
+      <View style={{ backgroundColor: "#eoDFC4", borderRadius: 20, borderColor: "33332E", alignItems: 'Center' }}>
+        <Text style={{ fontSize: 20}}>Status: Em Tratamento  </Text>
+      </View>
+      <View></View>
+      <View></View>
+      <View></View>
+    </View>
+  );
+}
+function CalendarioScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <View style={styles.logo}>
+        <Text style={{ fontSize: 40}}>Veterinaria</Text>
+        < Image style={styles.perfil_foto} source={{}} />
+      </View>
     </View>
   );
 }
@@ -31,6 +77,8 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Detalhes' }} />
+        <Stack.Screen name="Tratamento" component={TratamentoScreen} options={{ title: 'Tratamento' }} />
+        <Stack.Screen name="Calendario" component={CalendarioScreen} options={{ title: 'Calendario' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -43,4 +91,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textimput_cadastrologin: {
+    borderWidth: 3,
+    borderColor: "#000000",
+    borderRadius: 25,
+    padding: 10,
+    width: "80%",
+    margin: 10,
+  },
+  textimput_cadastrosenha: {
+    borderWidth: 3,
+    borderColor: "#000000",
+    borderRadius: 25,
+    padding: 10,
+    width: "80%",
+    margin: 10,
+  },
+  logo:{
+    margin: 15,
+  }
 });
